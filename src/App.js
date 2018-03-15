791f469d1511eead58f9e7f7051ed9d02f2b25fa
Frank Hangler <fhangler@gmail.com>
2018-03-15 11:16:22 -0700

    Basic page outline

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import './App.css'
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App container-fluid'>
+        <div className='row'>
+          <div className='col'>
+            <h1>Workforce Profiles</h1>
+          </div>
+        </div>
       </div>
     )
   }
