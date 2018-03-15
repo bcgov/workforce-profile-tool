bb3a118211d5f9968a994201520ea7fb675a461f
Frank Hangler <fhangler@gmail.com>
2018-03-15 10:58:59 -0700

    Remove includes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './bootstrap/bootstrap.css'
 import './App.css'
 
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
         </header>
         <p className='App-intro'>
