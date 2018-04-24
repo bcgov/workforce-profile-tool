ec7edb3c0c9da1eae499feb4177988677554701d
Frank Hangler <fhangler@gmail.com>
2018-04-23 22:35:23 -0700

    Extremely basic table for testing

--- src/Views/Occupation.js
+++ src/Views/Occupation.js
@@ -3,8 +3,31 @@ import React, { Component } from 'react'
 class Occupation extends Component {
   render () {
     console.log('data', this.props.data)
+
+    const headerRow = this.props.data
+      ? <tr>{Object.keys(this.props.data[0]).map(k => <th>{k}</th>)}</tr>
+      : null
+
+    const rows = this.props.data ? this.props.data.map(d => {
+      return (
+        <tr>
+          {Object.values(d).map(col => <td>{col}</td>)}
+        </tr>
+      )
+    }) : null
+
     return (
-      <h1>Occupation</h1>
+      <div>
+        <h1>Occupation</h1>
+        <table>
+          <thead>
+            {headerRow}
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
