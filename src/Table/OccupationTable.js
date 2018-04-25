cd534ee8849c8c523042111fdc6dcdb79800a912
Frank Hangler <fhangler@gmail.com>
2018-04-25 12:32:21 -0700

    Mock OccupationTable

---
+++ src/Table/OccupationTable.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Reactor from '@plot-and-scatter/reactor-table'
+
+class OccupationTable extends Component {
+  render () {
+    const dataRows = [
+      { 'key': 1, 'name': 'First' },
+      { 'key': 2, 'name': 'Second' },
+      { 'key': 3, 'name': 'Third' },
+      { 'key': 10, 'name': 'Tenth' },
+      { 'key': 11, 'name': 'Eleventh' }
+    ]
+
+    const columns = [{
+      id: 'id',
+      name: 'ID',
+      accessor: d => d.key,
+      displayAccessor: d => d.key
+    },
+    {
+      id: 'name',
+      name: 'Name',
+      accessor: d => d.name
+    }]
+
+    const rowFilter = (r) => true
+
+    return (
+      <div className='Table row'>
+        <div className='col'>
+          <Reactor.Table
+            columns={columns}
+            rows={dataRows}
+            rowFilter={rowFilter}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default OccupationTable
--- src/Views/Occupation.js
+++ src/Views/Occupation.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react'
+import OccupationTable from '../Table/OccupationTable'
 
 class Occupation extends Component {
-  generateKey (d, colKey = null) {
-    let key = `${d.BCPS_Ministry_Group}-${d.DesignatedMinority_Group}-${d.Employee_Type}-${d.Occupation_Region_Group}`
-
-    if (colKey) {
-      key += `-${colKey}`
-    }
-
-    return key
-  }
-
   render () {
-    console.log('data', this.props.data)
-
-    const headerRow = this.props.data && this.props.data.length > 0
-      ? <tr>{Object.keys(this.props.data[0]).map(k => <th key={k}>{k}</th>)}</tr>
-      : null
-
-    const rows = this.props.data ? this.props.data.map(d => {
-      return (
-        <tr key={this.generateKey(d)}>
-          {Object.keys(d).map(colKey => {
-            const col = d[colKey]
-            return <td key={this.generateKey(d, colKey)}>{col}</td>
-          })
-          }
-        </tr>
-      )
-    }) : null
-
     return (
       <div>
         <h1>Occupation</h1>
-        <table>
-          <thead>
-            {headerRow}
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
+        <OccupationTable data={this.props.data} />
       </div>
     )
   }
