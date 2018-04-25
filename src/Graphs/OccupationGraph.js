62db3b4f0e9b6872055052c04b9d60d4c6cee0bd
Frank Hangler <fhangler@gmail.com>
2018-04-25 15:22:08 -0700

    Rename

---
+++ src/Graphs/OccupationGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class OccupationGraph extends Component {
+  render () {
+    return (
+      <h1>Occupation</h1>
+    )
+  }
+}
+
+export default OccupationGraph
