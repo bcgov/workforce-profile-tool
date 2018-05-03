07965fe9a5723bba86fb26a4b296d07f8b976db5
Frank Hangler <fhangler@gmail.com>
2018-05-03 01:33:17 -0700

    Test for missing key

--- src/Core/App.js
+++ src/Core/App.js
@@ -29,6 +29,12 @@ class App extends Component {
       return data
     }
 
+    // If data is empty, don't filter
+    if (!data[0][key]) {
+      console.warn(`processFilter: key '${key}' does not exist in 0th data item`)
+      return data
+    }
+
     // If the value is ALL, don't filter
     if (value === ALL_VALUE) return data
 
@@ -69,6 +75,10 @@ class App extends Component {
       const iopReportData = this.processFilters(filters, this.state.originalData.iopReportData)
       this.setState({ iopReportData })
     }
+    if (Object.keys(filters).length > 0 && this.state.originalData.comparisonData) {
+      const comparisonData = this.processFilters(filters, this.state.originalData.comparisonData)
+      this.setState({ comparisonData })
+    }
   }
 
   async componentDidMount () {
@@ -84,7 +94,6 @@ class App extends Component {
     ministryData.forEach(r => { r.key = ''.concat(Object.values(r)) })
     occupationRegionData.forEach(r => { r.key = ''.concat(Object.values(r)) })
     flowReportData.forEach(r => { r.key = ''.concat(Object.values(r)) })
-    console.log('ministryData', ministryData)
 
     this.setState({
       iopReportData,
