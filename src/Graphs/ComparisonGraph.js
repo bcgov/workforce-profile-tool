0cb531dc111bb2b566d16cb3b08fe7537f94c300
Frank Hangler <fhangler@gmail.com>
2018-05-08 14:36:39 -0700

    Basic bullet graph for comparison

--- src/Graphs/ComparisonGraph.js
+++ src/Graphs/ComparisonGraph.js
@@ -12,23 +12,48 @@ class RegionGraph extends Component {
   render () {
     if (!this.props.data) return <div>Loading...</div>
 
+    // const chartData = this.props.data.map(d => {
+    //   const values = [
+    //     +d.BCPS,
+    //     +d.Available_Workforce,
+    //     +d.Employees_BC_Population
+    //   ]
+
+    //   return {
+    //     category: displayNameByKey('Des_Grp', d['Des_Grp']),
+    //     values
+    //   }
+    // })
+
     const chartData = this.props.data.map(d => {
-      const values = [
-        +d.BCPS,
-        +d.Available_Workforce,
-        +d.Employees_BC_Population
+      const count = +d.BCPS
+      const comparators = [
+        { value: +d.Available_Workforce, color: '#6b747c' },
+        { value: +d.Employees_BC_Population, color: '#d3e2ef' }
       ]
 
       return {
         category: displayNameByKey('Des_Grp', d['Des_Grp']),
-        values
+        color: '#70CCDB',
+        count,
+        comparators
       }
     })
 
     const graph = (
-      <PlusPlot.GroupedBarChart
+      // <PlusPlot.GroupedBarChart
+      //   data={chartData}
+      //   colors={['#70CCDB', '#D2E2EE', '#6c757d']}
+      //   options={{
+      //     height: 500,
+      //     dataLabels: { position: 25, formatter: (d) => formatPercent(d / 100, 1) },
+      //     margins: { top: 0, left: 125, bottom: 40, right: 40 },
+      //     axes: { yAxisLabel: '', xAxisLabel: '% representation' },
+      //     font: 'Myriad Pro'
+      //   }}
+      // />
+      <PlusPlot.BulletBarChart
         data={chartData}
-        colors={['#70CCDB', '#D2E2EE', '#6c757d']}
         options={{
           height: 500,
           dataLabels: { position: 25, formatter: (d) => formatPercent(d / 100, 1) },
@@ -36,6 +61,7 @@ class RegionGraph extends Component {
           axes: { yAxisLabel: '', xAxisLabel: '% representation' },
           font: 'Myriad Pro'
         }}
+        xLines={[]}
       />
     )
 
