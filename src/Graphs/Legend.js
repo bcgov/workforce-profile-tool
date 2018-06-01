fdc3f3c78fc087364e7199c6e5adf1f211867e66
Frank Hangler <fhangler@gmail.com>
2018-06-01 13:46:48 -0700

    Add tooltips to legends

--- src/Graphs/Legend.js
+++ src/Graphs/Legend.js
@@ -1,19 +1,42 @@
+/* global $ */
+
 import React, { Component } from 'react'
 
 import './Legend.css'
 
 class Legend extends Component {
+  constructor (props) {
+    super(props)
+    this.infoButtons = []
+  }
+
+  componentDidMount () {
+    this.infoButtons.forEach(infoButton => {
+      if (infoButton) $(infoButton).tooltip()
+    })
+  }
+
   render () {
-    const rows = this.props.items.map(i => {
+    const rows = this.props.items.map((item, index) => {
       return (
-        <tr key={i.color}>
+        <tr key={item.color}>
           <td className='color'>
             <div className='swatch'>
-              <span style={{ 'color': i.color }}>●</span>
+              <span style={{ 'color': item.color }}>●</span>
             </div>
           </td>
           <td className='label'>
-            {i.label}
+            {item.label}
+            {item.tooltip &&
+              <a className='Info'
+                ref={infoButton => { this.infoButtons[index] = infoButton }}
+                data-toggle='tooltip'
+                title={item.tooltip}
+                data-placement='bottom'
+              >
+                <i className='fas fa-info-circle' />
+              </a>
+            }
           </td>
         </tr>
       )
