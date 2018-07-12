7c24186889180ec537a075d0256d2a87037705a1
Frank Hangler <fhangler@gmail.com>
2018-07-12 16:06:08 -0700

    Remove console.log

--- src/Tabs/Tab.js
+++ src/Tabs/Tab.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 class Tab extends React.Component {
   render () {
-    console.log('Tab rendering')
     return (
       <div className='col-xs-12'>
         {this.props.children}
