913705933fc1cb5438fcfb7041ce28255de980c1
Frank Hangler <fhangler@gmail.com>
2018-07-19 14:39:43 -0700

    Latest Progress data

--- src/Data/DataLoader.js
+++ src/Data/DataLoader.js
@@ -17,7 +17,7 @@ class DataLoader {
   }
 
   static async getIndicatorsOfProgressReport () {
-    const url = `${DATA_PATH_BASE_2018}WP2018_Ind_Progress-2_UpdJUL17.csv`
+    const url = `${DATA_PATH_BASE_2018}WP2018_Ind_Progress-2_UpdJUL19.csv`
     return DataLoader.loadCSV(url)
   }
 
