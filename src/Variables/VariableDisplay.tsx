22cf6426bca47707080f296f49102624eb7b4599
Frank Hangler <frank@plotandscatter.com>
2021-02-09 01:03:46 -0800

    Show variable list

--- src/Variables/VariableDisplay.tsx
+++ src/Variables/VariableDisplay.tsx
@@ -4,52 +4,42 @@ import './VariableDisplay.scss'
 import FixTypeLater from '../@types/FixTypeLater'
 
 interface Props {
-  activeVariables: FixTypeLater
-  lockVariables: FixTypeLater
-  updateCallback: FixTypeLater
-  useShortDisplay?: FixTypeLater
   variableGroup: FixTypeLater
+  useShortDisplay?: boolean
 }
 
-class VariableDisplay extends Component<Props> {
-  render(): JSX.Element {
-    const options = this.props.variableGroup.selectableVariables.map(
-      (variable: FixTypeLater) => {
-        const variableGroupKey = this.props.variableGroup.key
-        const variableKey = variable.key
-        const isActive = isVariableActive(
-          this.props.activeVariables,
-          variableGroupKey,
-          variableKey
-        )
-        const isLocked = this.props.lockVariables
-        // console.log('isLocked', isLocked)
-        return (
-          <li
-            key={variable.key}
-            className={`${isActive ? ' active' : ''} ${
-              isLocked ? ' locked' : ''
-            }`}
-            onClick={() => {
-              if (!isLocked)
-                this.props.updateCallback(this.props.variableGroup, variable)
-            }}
-          >
-            {this.props.useShortDisplay
-              ? variable.shortDisplay
-              : variable.display}
-          </li>
-        )
-      }
-    )
+const VariableDisplay = ({
+  useShortDisplay,
+  variableGroup,
+}: Props): JSX.Element => {
+  const options = variableGroup.selectableVariables.map(
+    (variable: FixTypeLater) => {
+      const variableGroupKey = variableGroup.key
+      const variableKey = variable.key
+      const isActive = true // TODO: fix this
+      const isLocked = false // TODO: fix this
+      return (
+        <li
+          key={variable.key}
+          className={`${isActive ? ' active' : ''} ${
+            isLocked ? ' locked' : ''
+          }`}
+          onClick={() => {
+            console.log('Toggle') // TODO: fix this
+          }}
+        >
+          {useShortDisplay ? variable.shortDisplay : variable.display}
+        </li>
+      )
+    }
+  )
 
-    return (
-      <div className="VariableDisplay">
-        <h3>{this.props.variableGroup.display}</h3>
-        <ul>{options}</ul>
-      </div>
-    )
-  }
+  return (
+    <div className="VariableDisplay">
+      <h3>{variableGroup.display}</h3>
+      <ul>{options}</ul>
+    </div>
+  )
 }
 
 export default VariableDisplay
--- src/Variables/VariableList.tsx
+++ src/Variables/VariableList.tsx
@@ -3,36 +3,20 @@ import { RouteComponentProps, withRouter } from 'react-router-dom'
 import './VariableList.scss'
 import VariableDisplay from './VariableDisplay'
 import FixTypeLater from '../@types/FixTypeLater'
+import { VARIABLE_MANAGER } from './VariableManager'
 
-interface Props extends RouteComponentProps {
-  activeVariables?: FixTypeLater
-  updateVariable?: FixTypeLater
-  variablesToLock?: FixTypeLater
-  variableManager?: FixTypeLater
-}
-
-class VariableList extends Component<Props> {
-  render(): JSX.Element {
-    // const variables = this.props.variableManager.variableGroups.map(
-    //   (group: FixTypeLater) => (
-    //     <VariableDisplay
-    //       key={group.key}
-    //       variableGroup={group}
-    //       activeVariables={this.props.activeVariables}
-    //       updateCallback={this.props.updateVariable}
-    //       lockVariables={this.props.variablesToLock[group.key]}
-    //       // useShortDisplay={group.key === 'Ministry_Key'}
-    //     />
-    //   )
-    // )
-    const variables: FixTypeLater[] = []
-
-    return (
-      <div className="VariableList row">
-        <div className="col">{variables}</div>
-      </div>
+const VariableList = (): JSX.Element => {
+  const variables = VARIABLE_MANAGER.variableGroups.map(
+    (group: FixTypeLater) => (
+      <VariableDisplay key={group.key} variableGroup={group} />
     )
-  }
+  )
+
+  return (
+    <div className="VariableList row">
+      <div className="col">{variables}</div>
+    </div>
+  )
 }
 
 export default withRouter(VariableList)
