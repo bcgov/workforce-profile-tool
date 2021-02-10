91bf97b2ecd9f883200106f9f7f1502a51e61f9a
Frank Hangler <frank@plotandscatter.com>
2021-02-09 16:48:05 -0800

    Add scale helper

---
+++ src/Services/scales.ts
@@ -0,0 +1,21 @@
+import * as d3 from 'd3'
+import Dictionary from '../@types/Dictionary'
+
+export const ticks = <T extends Dictionary>(
+  data: T[],
+  keys: string[]
+): number[] => {
+  const domainValues: number[] = []
+
+  data.forEach((d) => {
+    Object.keys(d).forEach((k) => {
+      if (keys.includes(k)) {
+        domainValues.push(d[k])
+      }
+    })
+  })
+
+  const max = d3.max(domainValues) as number
+  const scale = d3.scaleLinear().domain([0, max]).range([0, max])
+  return scale.nice().ticks()
+}
