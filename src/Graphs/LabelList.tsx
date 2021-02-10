cd478053f824d5c4dc92aeed92dce75ee756cf88
Frank Hangler <frank@plotandscatter.com>
2021-02-09 16:48:12 -0800

    Wrap LabelList

---
+++ src/Graphs/LabelList.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import * as R from 'recharts'
+
+interface Props {
+  dataKey: string
+  formatter?: (d: string) => string
+  position?: string
+}
+
+const LabelList = (props: Props): JSX.Element => {
+  const position = props.position || 'right'
+  return (
+    <R.LabelList
+      {...props}
+      dataKey={props.dataKey}
+      // @ts-ignore The typings are wrong
+      formatter={props.formatter}
+      position={position}
+      style={{
+        fontSize: '14px',
+      }}
+    />
+  )
+}
+
+export default LabelList
