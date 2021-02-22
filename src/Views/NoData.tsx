fd7c3436a511242b6c526148ee8dfe348856addb
Frank Hangler <frank@plotandscatter.com>
2021-02-22 09:06:05 -0800

    Basic types

--- src/Views/NoData.tsx
+++ src/Views/NoData.tsx
@@ -1,26 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import FixTypeLater from '../@types/FixTypeLater'
 import { VARIABLE_MANAGER } from '../Variables/VariableManager'
 
-class Loading extends Component {
-  render(): JSX.Element {
-    let groupNames: FixTypeLater = VARIABLE_MANAGER.variableGroups.map(
-      (g) => g.display
-    )
-    groupNames[groupNames.length - 1] = `or ${
-      groupNames[groupNames.length - 1]
-    }`
-    groupNames = groupNames.join(', ')
-    return (
-      <div className="alert alert-warning" role="alert">
-        <h2>No data</h2>
-        <p>
-          There is no data for the selected combination of filters. Try changing
-          the selected {groupNames}.
-        </p>
-      </div>
-    )
-  }
+const NoData = (): JSX.Element => {
+  let groupNames: FixTypeLater = VARIABLE_MANAGER.variableGroups.map(
+    (g) => g.display
+  )
+  groupNames[groupNames.length - 1] = `or ${groupNames[groupNames.length - 1]}`
+  groupNames = groupNames.join(', ')
+  return (
+    <div className="alert alert-warning" role="alert">
+      <h2>No data</h2>
+      <p>
+        There is no data for the selected combination of filters. Try changing
+        the selected {groupNames}.
+      </p>
+    </div>
+  )
 }
 
-export default Loading
+export default NoData
