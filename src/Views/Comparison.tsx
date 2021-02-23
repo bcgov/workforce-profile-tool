13f1380496d9c39e7dea360b0d2fdba9af84253f
Frank Hangler <frank@plotandscatter.com>
2021-02-23 12:38:09 -0800

    Refactor Comparison table

--- src/Views/Comparison.tsx
+++ src/Views/Comparison.tsx
@@ -1,29 +1,52 @@
-import { withRouter } from 'react-router-dom'
 import React from 'react'
 
+import { ColumnWithClassName } from '../@types/ColumnWithClassName'
+import { ComparisonRawData } from '../@types/DataTypes'
+import { formatPercent } from '../Services/formatter'
+import { StringParam, useQueryParam } from 'use-query-params'
 import { useDataManager } from '../Data/DataManager'
-import ComparisonTable from '../Table/ComparisonTable'
-import Loading from './Loading'
-import NoData from './NoData'
-import Title from './Title'
+import { VARIABLES } from '../Variables/VariableManager'
+import GenericTable from '../Table/GenericTable'
+import GenericView from './GenericView'
 
 const Comparison = (): JSX.Element => {
-  const employeeCount = 1000 // useQueryParams
-  const { progressData: data } = useDataManager()
+  const { comparisonData: data } = useDataManager()
+
+  const [ministryKey] = useQueryParam('Ministry_Key', StringParam)
+  const ministry = VARIABLES.displayNameByKey('Ministry_Key', ministryKey) // TODO: cleaner implementation of this
+
+  const columns: ColumnWithClassName<ComparisonRawData>[] = [
+    {
+      id: 'Des_Grp',
+      Header: 'Designated Group',
+      accessor: (d) =>
+        VARIABLES.displayNameByKey('Des_Grp', d['Des_Grp']) || '',
+    },
+    {
+      id: 'Employees_BCPS',
+      Header: `${ministry}, %`,
+      accessor: (d) => formatPercent(d['Employees_BCPS'], 1, 100),
+      className: 'text-right',
+    },
+    {
+      id: 'Available_Workforce_BCPS',
+      Header: 'Available Workforce, %',
+      accessor: (d) => formatPercent(d['Available_Workforce_BCPS'], 1, 100),
+      className: 'text-right',
+    },
+    {
+      id: 'Employees_BC_Population',
+      Header: 'BC Population, %',
+      accessor: (d) => formatPercent(d['Employees_BC_Population'], 1, 100),
+      className: 'text-right',
+    },
+  ]
 
   return (
-    <div>
-      <Title title={'Comparison with Provincial Workforce'} />
-      {!data && <Loading />}
-      {data && data.length === 0 && <NoData />}
-      {data && data.length > 0 && (
-        <div>
-          {/* <ComparisonGraph data={data} title={title} ministry={ministry} /> */}
-          <ComparisonTable />
-        </div>
-      )}
-    </div>
+    <GenericView title={'Comparison with Provincial Workforce'} data={data}>
+      <GenericTable data={data} columns={columns} filename={'comparison'} />
+    </GenericView>
   )
 }
 
-export default withRouter(Comparison)
+export default Comparison
