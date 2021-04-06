a542cf6b40de89cdb5897bb51735e1a45aaaa582
Frank Hangler <frank@plotandscatter.com>
2021-04-06 12:18:25 -0700

    Add BC Stats logo. Fix #111

--- src/Core/Header.tsx
+++ src/Core/Header.tsx
@@ -2,12 +2,15 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 
 import './Header.scss'
+import logo from './bc-stats-logo.png'
 
 const Header = (): JSX.Element => {
   return (
     <div className="Header row">
-      <div className="col">
+      <div className="col mb-4 text-center">
         <Link to={'/'}>
+          <img src={logo} style={{ maxWidth: '100%' }} />
+          <br />
           <h1>Workforce Profiles</h1>
         </Link>
       </div>
