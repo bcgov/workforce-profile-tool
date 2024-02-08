0bb621317763b4d7dcc3a4a3fbacea9c395c2b39
Dan Ebenal <dan.ebenal@gov.bc.ca>
2024-02-08 13:21:52 -0800

    rm unused deployment scripts

--- openshift/deploy_vars.example.sh
+++
@@ -1,9 +0,0 @@
-# Update these and remove the .example in the filename.
-
-NAME="wfp"
-IMAGE_TAG="some-image-tag"
-TOOLS_NAMESPACE="abc123-tools"
-IMAGE_NAMESPACE=$TOOLS_NAMESPACE # Default to the same as tools; could change
-DEPLOY_NAMESPACE="abc123-tools"
-BASE_OPENSHIFT_URL="some.openshift.deployment.url"
-DO_BUILD=true
