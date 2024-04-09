0ff13fe9beaa953f2126c128381cd0c30893ba1f
James Elson <james.elson@gov.bc.ca>
2024-04-09 16:31:22 -0700

    Added remaining tests

--- frontend/cypress/tests/progress.cy.ts
+++ frontend/cypress/tests/progress.cy.ts
@@ -4,21 +4,31 @@ const downloadsFolder = Cypress.config('downloadsFolder')
 describe('test indicators of progress page', () => {
   it('clicking "Indicators of Progress" navigates to a new url', () => {
     cy.visit('/')
-
-    // Click Indicators of Progress tab at the top
-    // Verify you are on the correct page
-    // Verify url includes /indicators-of-progress
+    cy.contains('Indicators of Progress').click()
+    cy.contains('Indicators of Progress — By Designated Group')
+    cy.url().should('include', '/indicators-of-progress')
   })
 
-  it('verify downloading a PDF', () => {
+  it('verify downloading a PNG', () => {
     cy.visit('/#/indicators-of-progress')
-
-    // Save a PNG, and verify existance
+    cy.contains('Save as PNG').click()
+    cy.readFile(path.join(downloadsFolder, 'Progress.png'))
   })
 
   it('verify downloading a CSV', () => {
     cy.visit('/#/indicators-of-progress')
+    cy.contains('Export data to CSV').click()
+    cy.readFile(path.join(downloadsFolder, 'progress.csv'))
+  })
 
-    // Save a CSV, and verify existance
+  it('verify items are not clickable', () => {
+    cy.visit('/#/organizations')
+    cy.get('div')
+      .should('have.class', 'Ministry_Key')
+      .children()
+      .find('li')
+      .contains('BC Public Service')
+      .nextAll()
+      .should('have.class', 'locked')
   })
 })
