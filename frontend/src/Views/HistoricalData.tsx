cc3b3a322706f70eaa96bb50f41a5277881d9a85
Dan Ebenal <dan.ebenal@gov.bc.ca>
2024-09-10 10:26:00 -0700

    PSRE-1725: Update historical data text

--- frontend/src/Views/HistoricalData.tsx
+++ frontend/src/Views/HistoricalData.tsx
@@ -1,7 +1,9 @@
 import { VARIABLE_MAP, useDataManager } from '../Data/DataManager'
+import { Link, useLocation } from 'react-router-dom'
 
 const HistoricalData = (): JSX.Element => {
   const { year } = useDataManager()
+  const location = useLocation()
 
   if (year === VARIABLE_MAP['Year'].variables.slice(-1).pop()?.key) {
     return <></>
@@ -10,7 +12,9 @@ const HistoricalData = (): JSX.Element => {
   return (
     <div className="alert alert-warning Shadow" role="alert">
       <h2>Historical data</h2>
-      <p>You are currently viewing data from {year}.</p>
+      <p>You are currently viewing data from {year}. Caution is advised when comparing the current report to historical reports.
+        For more information refer to the <Link to={`/${location.search}`}>Home Page</Link>, or contact us at <a href="mailto:work.environment.survey@gov.bc.ca">BC Stats</a>.
+      </p>
     </div>
   )
 }
